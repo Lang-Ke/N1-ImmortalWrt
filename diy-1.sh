e7304906f466276c37ae2c286b433f28d415e47e
Lang-Ke <80813300+Lang-Ke@users.noreply.github.com>
2026-06-07 20:02:05 +0800

    Update diy-1.sh

--- diy-1.sh
+++ diy-1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 添加第三方 feeds
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-# echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+ echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-# ./scripts/feeds update -a
-# ./scripts/feeds install -a
+ ./scripts/feeds update -a
+ ./scripts/feeds install -a
